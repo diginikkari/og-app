41915a43268785abd0cf69d444d3784630293b5a
Timo Santi <timo.santi@santicon.fi>
2023-06-05 14:44:09 +0300

    Add og api route and outputFileTracing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    outputFileTracingExcludes: {
+      '**/*': ['node_modules/**/@swc/core*'],
+    }, // See https://github.com/vercel/next.js/issues/42641#issuecomment-1320713368
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
